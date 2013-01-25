30cdcd3a2ecdce25ca9f18b322e53c96ae2b9cdf
Under Hyp3d <cavejohnsonhere@ya.ru>
2013-01-25 01:13:09 +0100

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp3000::Application.config.secret_token = '1c7871a5ace54c4901f5cd54762b8733f48aeb55a14b657e0ce213bd3e411838305bc922920dd4a0e77be8078d9d4ff8c300a21bd76c3aa249b418a475e93cb8'
